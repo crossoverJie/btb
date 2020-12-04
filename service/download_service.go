95d34bb3264963dbf3f904fae7e377b9e2158ab3
crossoverJie <crossoverjie@gmail.com>
2020-12-05 01:00:15 +0800

    multi thread download_service

--- service/download_service.go
+++ service/download_service.go
@@ -5,37 +5,45 @@ import (
 	"fmt"
 	"github.com/fatih/color"
 	"log"
+	"sync"
 )
 
 func DownLoadPic(markdownPath, downloadPath string) {
-
+	wg := sync.WaitGroup{}
 	allFile, err := util.GetAllFile(markdownPath)
+	wg.Add(len(*allFile))
+
 	if err != nil {
 		log.Fatal("read file error")
 	}
 
 	for _, filePath := range *allFile {
-		picCount := 0
-		allLine, err := util.ReadFileLine(filePath)
-		if err != nil {
-			log.Fatal(err)
-		}
-		for _, line := range *allLine {
-			url, err := util.MatchPicture(&line)
-			if err == nil && *url != "" {
-				err := util.DownloadFile(*url, *genFullFileName(downloadPath, filePath, url))
-				if err != nil {
-					log.Fatal(err)
-				}
-				fmt.Printf("Download: %v image: %v successful.\n", filePath, *url)
-				picCount++
+
+		go func(filePath string) {
+			picCount := 0
+			allLine, err := util.ReadFileLine(filePath)
+			if err != nil {
+				log.Fatal(err)
 			}
+			for _, line := range *allLine {
+				url, err := util.MatchPicture(&line)
+				if err == nil && *url != "" {
+					err := util.DownloadFile(*url, *genFullFileName(downloadPath, filePath, url))
+					if err != nil {
+						log.Fatal(err)
+					}
+					fmt.Printf("Download: %v image: %v successful.\n", filePath, *url)
+					picCount++
+				}
 
-		}
-		color.Green("Run [%v], [%v]images successfully!!!", filePath, picCount)
-
+			}
+			color.Green("Run [%v], [%v]images successfully!!!", filePath, picCount)
+			wg.Done()
+		}(filePath)
 	}
+	wg.Wait()
 	color.Green("Run [%v] size successfully!!!", len(*allFile))
+
 	if err != nil {
 		log.Fatal(err)
 	}
